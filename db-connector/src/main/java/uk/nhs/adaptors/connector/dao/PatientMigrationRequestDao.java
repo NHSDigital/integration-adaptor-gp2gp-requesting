9b6ac541d59e2c4777d0ef64dbba6f553547163d
hospel <12736698+ole4ryb@users.noreply.github.com>
2025-01-27 16:50:45 +0000

    initial changes

--- db-connector/src/main/java/uk/nhs/adaptors/connector/dao/PatientMigrationRequestDao.java
+++ db-connector/src/main/java/uk/nhs/adaptors/connector/dao/PatientMigrationRequestDao.java
@@ -1,6 +1,8 @@
 package uk.nhs.adaptors.connector.dao;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindList;
@@ -11,36 +13,41 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import uk.nhs.adaptors.common.enums.MigrationStatus;
 import uk.nhs.adaptors.connector.model.PatientMigrationRequest;
 
-public interface PatientMigrationRequestDao {
+public abstract class PatientMigrationRequestDao {
 
     @SqlUpdate("insert_patient_migration_request")
     @UseClasspathSqlLocator
-    void addNewRequest(@Bind("nhsNumber") String patientNhsNumber, @Bind("conversationId") String conversationId,
+    public abstract void addNewRequest(@Bind("nhsNumber") String patientNhsNumber, @Bind("conversationId") String conversationId,
         @Bind("losingOdsCode") String losingOdsCode, @Bind("winningOdsCode") String winningOdsCode);
 
     @SqlQuery("select_patient_migration_request")
     @UseClasspathSqlLocator
-    PatientMigrationRequest getMigrationRequest(@Bind("conversationId") String conversationId);
+    public abstract PatientMigrationRequest getMigrationRequest(@Bind("conversationId") String conversationId);
 
     @SqlQuery("select_patient_migration_request_id")
     @UseClasspathSqlLocator
-    int getMigrationRequestId(@Bind("conversationId") String conversationId);
+    @Deprecated
+    abstract int getMigrationRequestId(@Bind("conversationId") String conversationId);
+
+    public int getMigrationRequestId(@Bind("conversationId") UUID conversationId) {
+        return this.getMigrationRequestId(conversationId.toString().toUpperCase(Locale.ROOT));
+    }
 
     @SqlUpdate("save_bundle_resource_and_inbound_message_data")
     @UseClasspathSqlLocator
-    void saveBundleAndInboundMessageData(@Bind("conversationId") String conversationId, @Bind("bundle") String bundle,
+    public abstract void saveBundleAndInboundMessageData(@Bind("conversationId") String conversationId, @Bind("bundle") String bundle,
         @Bind("inboundMessage") String inboundMessage);
 
     @SqlQuery("exists_by_conversation_id")
     @UseClasspathSqlLocator
-    boolean existsByConversationId(@Bind("conversationId") String conversationId);
+    public abstract boolean existsByConversationId(@Bind("conversationId") String conversationId);
 
     @SqlQuery("select_patient_migration_request_by_patient_nhs_number")
     @UseClasspathSqlLocator
-    PatientMigrationRequest getLatestMigrationRequestByPatientNhsNumber(@Bind("patientNhsNumber") String patientNhsNumber);
+    public abstract PatientMigrationRequest getLatestMigrationRequestByPatientNhsNumber(@Bind("patientNhsNumber") String patientNhsNumber);
 
     @SqlQuery("select_patient_migration_requests_by_latest_migration_status")
     @UseClasspathSqlLocator
-    List<PatientMigrationRequest> getMigrationRequestsByLatestMigrationStatusIn(
+    public abstract List<PatientMigrationRequest> getMigrationRequestsByLatestMigrationStatusIn(
         @BindList("statusList") List<MigrationStatus> statusList);
 }
