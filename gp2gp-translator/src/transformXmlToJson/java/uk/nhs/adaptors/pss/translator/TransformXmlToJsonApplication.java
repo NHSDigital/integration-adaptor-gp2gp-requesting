d8756f56ff527321f51c55f34a56cb6d1ae6c45e
MartinWheelerMT <martin.wheeler@madetech.com>
2025-07-01 09:21:28 +0100

    * Add additional test

--- gp2gp-translator/src/transformXmlToJson/java/uk/nhs/adaptors/pss/translator/TransformXmlToJsonApplication.java
+++ gp2gp-translator/src/transformXmlToJson/java/uk/nhs/adaptors/pss/translator/TransformXmlToJsonApplication.java
@@ -4,6 +4,7 @@ import jakarta.xml.bind.JAXBException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.apache.commons.io.FilenameUtils;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.v3.RCMRIN030000UKMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +15,19 @@ import uk.nhs.adaptors.common.util.fhir.FhirParser;
 import uk.nhs.adaptors.pss.translator.exception.BundleMappingException;
 import uk.nhs.adaptors.pss.translator.service.BundleMapperService;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallString;
 
@@ -28,7 +35,11 @@ import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallSt
 @Slf4j
 @SpringBootApplication
 public class TransformXmlToJsonApplication implements CommandLineRunner {
-    public static final String RESOURCES_FOLDER = "src/transformXmlToJson/resources/";
+    public static final String XML_INPUT_PATH =
+        Paths.get("gp2gp-translator/src/").toFile().getAbsoluteFile().getAbsolutePath() + "/transformXmlToJson/resources/input/";
+
+    public static final String JSON_OUTPUT_PATH =
+        Paths.get("gp2gp-translator/src/").toFile().getAbsoluteFile().getAbsolutePath() + "/transformXmlToJson/resources/output/";
 
     private final FhirParser fhirParser;
     private final BundleMapperService bundleMapperService;
@@ -39,21 +50,58 @@ public class TransformXmlToJsonApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Arrays.stream(new File(RESOURCES_FOLDER + "input").listFiles())
+        getInputFilePaths().forEach(this::convertXmlToJsonAndSaveToOutputFolder);
+    }
+
+    private List<Path> getInputFilePaths() {
+        var path = Paths.get(XML_INPUT_PATH);
+
+        if (!Files.isDirectory(path) || !Files.isReadable(path)) {
+            throw new NoXmlFilesFound("Input folder is missing or unreadable: " +  XML_INPUT_PATH);
+        }
+
+
+        File[] files = path.toFile()
+            .listFiles((dir, name) -> name.toLowerCase().endsWith(".xml"));
+
+        if (files == null || files.length == 0) {
+            throw new NoXmlFilesFound("No XML files found in " + XML_INPUT_PATH);
+        }
+
+        return Arrays.stream(files)
             .map(File::toPath)
-            .filter(it ->  it.toString().endsWith("xml"))
-            .forEach(this::convertXmlToJsonAndSaveToOutputFolder);
+            .collect(Collectors.toList());
     }
 
     private void convertXmlToJsonAndSaveToOutputFolder(Path input) {
         try {
             RCMRIN030000UKMessage rcmrin030000UKMessage = unmarshallString(Files.readString(input), RCMRIN030000UKMessage.class);
-            String gpConnectOutput = fhirParser.encodeToJson(mapToBundle(rcmrin030000UKMessage));
-            Path outputPath = outputPath(input);
-            LOGGER.debug("Writing [{}] characters to [{}]", gpConnectOutput.length(), outputPath);
-            Files.writeString(outputPath, gpConnectOutput);
+            var mappedBundle = mapToBundle(rcmrin030000UKMessage);
+            String bundleJson = fhirParser.encodeToJson(mappedBundle);
+            writeToFile(bundleJson, input.getFileName().toString());
         } catch (JAXBException | IOException | BundleMappingException e) {
             LOGGER.debug("Unable to convert [{}]", input, e);
+        }
+    }
+
+    private void writeToFile(String bundleJson, String sourceFileName) {
+        var outputFileName = FilenameUtils.removeExtension(sourceFileName);
+
+        var path = Paths.get(JSON_OUTPUT_PATH);
+
+        if (!Files.isDirectory(path) || !Files.isReadable(path)) {
+            throw new NoXmlFilesFound("Output folder is missing or unreadable: " +  JSON_OUTPUT_PATH);
+        }
+
+        var outputFilePath = JSON_OUTPUT_PATH + outputFileName + ".json";
+        try (
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath, StandardCharsets.UTF_8))
+        ) {
+            LOGGER.debug("Writing [{}] characters to [{}]", bundleJson.length(), outputFilePath);
+            writer.write(bundleJson);
+            LOGGER.info("Contents of file: {}. Saved to: {}.json", sourceFileName, outputFileName);
+        } catch (IOException e) {
+            LOGGER.error("Could not send Xml result to the file", e);
         }
     }
 
@@ -61,12 +109,9 @@ public class TransformXmlToJsonApplication implements CommandLineRunner {
         return bundleMapperService.mapToBundle(rcmrin030000UKMessage, "AB03", List.of());
     }
 
-    private static Path outputPath(Path input) throws IOException {
-        Path fileName = input.getFileName();
-        if (fileName == null) {
-            throw new IOException("Input path missing fileName - " + input);
+    public static class NoXmlFilesFound extends RuntimeException {
+        public NoXmlFilesFound(String errorMessage) {
+            super(errorMessage);
         }
-        final var outputFilename = fileName.toString().replace(".xml", ".json");
-        return Paths.get(RESOURCES_FOLDER + "output/", outputFilename);
     }
 }
