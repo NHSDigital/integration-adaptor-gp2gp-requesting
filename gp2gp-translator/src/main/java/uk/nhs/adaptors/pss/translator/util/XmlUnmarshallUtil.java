a312d50a5207e680d5f615867fd7c2176a6094df
ORybak5 <12736698+ORybak5@users.noreply.github.com>
2026-01-12 11:58:41 +0000

    Caching unmarshaller

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/XmlUnmarshallUtil.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/XmlUnmarshallUtil.java
@@ -3,6 +3,8 @@ package uk.nhs.adaptors.pss.translator.util;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.File;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBElement;
@@ -16,11 +18,31 @@ import org.apache.commons.io.IOUtils;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class XmlUnmarshallUtil {
 
-    public static <T> T unmarshallFile(File xmlFile, Class<T> destinationClass) throws JAXBException {
-        Unmarshaller unmarshaller = createUnmarshaller(destinationClass);
-        JAXBElement<T> unmarshalledMessage = (JAXBElement) unmarshaller.unmarshal(xmlFile);
-        return unmarshalledMessage.getValue();
+    private static final ConcurrentMap<Class<?>, JAXBContext> CONTEXT_CACHE = new ConcurrentHashMap<>();
+
+    private static JAXBContext getContext(Class<?> clazz) {
+        return CONTEXT_CACHE.computeIfAbsent(clazz, c -> {
+            try {
+                return JAXBContext.newInstance(c);
+            } catch (JAXBException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
+
+    public static <T> T unmarshallFile(File xmlFile, Class<T> destinationClass) throws JAXBException {
+
+        Unmarshaller unmarshaller = getContext(destinationClass).createUnmarshaller();
+
+        Object result = unmarshaller.unmarshal(xmlFile);
+
+        if (result instanceof JAXBElement) {
+            return ((JAXBElement<T>) result).getValue();
+        }
+
+        return destinationClass.cast(result);
+    }
+
 
     public static <T> T unmarshallString(String xmlString, Class<T> destinationClass) throws JAXBException {
         Unmarshaller unmarshaller = createUnmarshaller(destinationClass);
