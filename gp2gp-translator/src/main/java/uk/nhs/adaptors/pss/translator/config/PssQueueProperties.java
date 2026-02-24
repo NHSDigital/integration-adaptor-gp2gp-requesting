13330a631782ed0da6d15682898cee14f624e5f4
Jon Hardy <Jonathan.Hardy97@gmail.com>
2026-02-24 15:45:09 +0000

    Pss Queue config validation (#1245)
    * when config contains all properties, context is created
    * when config has missing properties, context is not created
    *  Move dependency on `spring-web` from `common to `gpc-facade`.
    * Add explicit compile-time only references to `jakarta` and `lombok` to ensure dependecies are correctly resolved.
    * Add default values for integration tests for properties which are now validated.
    
    ---------
    
    Co-authored-by: MartinWheelerMT <martin.wheeler@madetech.com>

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/config/PssQueueProperties.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/config/PssQueueProperties.java
@@ -5,11 +5,15 @@ import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+import uk.nhs.adaptors.pss.translator.validation.ValidPssQueueProperties;
 
 @Component
 @ConfigurationProperties(prefix = "amqp.pss")
 @Getter
 @Setter
+@Validated
+@ValidPssQueueProperties
 public class PssQueueProperties {
     private String queueName;
     private String broker;
