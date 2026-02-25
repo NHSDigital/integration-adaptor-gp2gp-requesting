89f0dfdb37adcd9438b1d9461972188f9413cb38
Jon Hardy <Jonathan.Hardy97@gmail.com>
2026-02-25 10:12:11 +0000

    update imports

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/validation/ValidMhsQueueProperties.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/validation/ValidMhsQueueProperties.java
@@ -3,8 +3,11 @@ package uk.nhs.adaptors.pss.translator.validation;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.*;
-
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Documented
 @Constraint(validatedBy = MhsQueuePropertyValidator.class)
