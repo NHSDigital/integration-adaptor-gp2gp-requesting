56f7bb30064109f7b0921d695b72e1dc9e06b804
chiaramapellimt <chiara.mapelli@madetech.com>
2026-04-02 13:04:14 +0100

    NIAD-3432: Skeleton processing service now removes leading _ before performing XPath query

--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/service/SkeletonProcessingServiceTests.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/service/SkeletonProcessingServiceTests.java
@@ -6,7 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doThrow;
 import static uk.nhs.adaptors.common.util.FileUtil.readResourceAsString;
 
 import java.nio.charset.StandardCharsets;
