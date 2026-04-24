1d41d22605931a306f19c2b5363c4b42dd04c83b
Jon Hardy <Jonathan.Hardy97@gmail.com>
2026-04-24 14:46:33 +0100

    initial changes for testing

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/storage/AzureStorageService.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/storage/AzureStorageService.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 import java.util.Locale;
 
 import com.azure.storage.blob.BlobContainerClient;
---
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/storage/AzureStorageServiceTest.java
@@ -0,0 +1,62 @@
+package uk.nhs.adaptors.pss.translator.storage;
+
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.azure.AzuriteContainer;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class AzureStorageServiceTest {
+
+    private static final String CONTAINER_NAME = "AzureContainer";
+    private static final String FILE_NAME = "test-file.txt";
+
+    private AzureStorageService azureStorageService;
+    private AzuriteContainer azuriteContainer;
+    private BlobServiceClient blobServiceClient;
+    private StorageServiceConfiguration config;
+
+    @BeforeEach
+    void setUp() {
+        azuriteContainer = new AzuriteContainer("mcr.microsoft.com/azure-storage/azurite:3.33.0");
+        azuriteContainer.start();
+        System.out.println("AzuriteContainer started");
+
+        blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString(azuriteContainer.getConnectionString())
+                .buildClient();
+
+        config = new StorageServiceConfiguration();
+        config.setContainerName(CONTAINER_NAME);
+
+        azureStorageService = new AzureStorageService(config);
+    }
+
+    @AfterEach
+    void tearDown() {
+        azuriteContainer.stop();
+    }
+
+    @Test
+    void uploadToStorageTest() throws IOException {
+        String uploadContent = "upload-content";
+
+        azureStorageService.uploadFile(FILE_NAME, uploadContent.getBytes(StandardCharsets.UTF_8));
+
+        final var request = GetObjectRequest.builder().bucket(CONTAINER_NAME).key(FILE_NAME).build();
+        ResponseInputStream<GetObjectResponse> uploadedObjectInS3 = blobServiceClient.(request);
+        String uploadedS3Content = new String(uploadedObjectInS3.readAllBytes(), StandardCharsets.UTF_8);
+
+        assertEquals(uploadContent, uploadedS3Content);
+    }
+
+}
