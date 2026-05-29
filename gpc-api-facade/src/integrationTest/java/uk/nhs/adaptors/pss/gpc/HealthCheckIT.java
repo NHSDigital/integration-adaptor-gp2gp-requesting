07c58e7617b94f73b22c33a611ba6aff06355de7
ORybak5 <12736698+ORybak5@users.noreply.github.com>
2026-05-29 11:29:52 +0100

    introducing slices to GPC Api Facade tests to increase speed

--- gpc-api-facade/src/integrationTest/java/uk/nhs/adaptors/pss/gpc/HealthCheckIT.java
+++ gpc-api-facade/src/integrationTest/java/uk/nhs/adaptors/pss/gpc/HealthCheckIT.java
@@ -1,35 +1,31 @@
 package uk.nhs.adaptors.pss.gpc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith({SpringExtension.class})
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@DirtiesContext
+@SpringBootTest(classes = GpcFacadeApplication.class)
+@AutoConfigureMockMvc
 public class HealthCheckIT {
+
     private static final String HEALTHCHECK_ENDPOINT = "/healthcheck";
 
-    @LocalServerPort
-    private int port;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    public void When_GettingHealthCheck_Expect_ServiceIsUp() {
-        String response = WebClient.builder()
-            .baseUrl("http://localhost:" + port)
-            .build()
-            .get()
-            .uri(builder -> builder.path(HEALTHCHECK_ENDPOINT).build())
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
+    public void When_GettingHealthCheck_Expect_ServiceIsUp() throws Exception {
+        var response = mockMvc.perform(get(HEALTHCHECK_ENDPOINT))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
 
         assertThat(response).contains("UP");
     }
