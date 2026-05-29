3ef88fa1fe53edc6bfc8ff8054ff71df5a4959eb
ORybak5 <12736698+ORybak5@users.noreply.github.com>
2026-05-29 11:45:59 +0100

    rollback to the original version but without @DirtiesContext

--- gpc-api-facade/src/integrationTest/java/uk/nhs/adaptors/pss/gpc/HealthCheckIT.java
+++ gpc-api-facade/src/integrationTest/java/uk/nhs/adaptors/pss/gpc/HealthCheckIT.java
@@ -1,31 +1,35 @@
 package uk.nhs.adaptors.pss.gpc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@SpringBootTest(classes = GpcFacadeApplication.class)
+@SpringBootTest(classes = GpcFacadeApplication.class, webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
 public class HealthCheckIT {
 
     private static final String HEALTHCHECK_ENDPOINT = "/healthcheck";
 
-    @Autowired
-    private MockMvc mockMvc;
+    @LocalServerPort
+    private int port;
 
     @Test
-    public void When_GettingHealthCheck_Expect_ServiceIsUp() throws Exception {
-        var response = mockMvc.perform(get(HEALTHCHECK_ENDPOINT))
-            .andExpect(status().isOk())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
+    public void When_GettingHealthCheck_Expect_ServiceIsUp() {
+        var response = WebClient.builder()
+                .baseUrl("http://localhost:" + port)
+                .build()
+                .get()
+                .uri(builder -> builder.path(HEALTHCHECK_ENDPOINT).build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
 
         assertThat(response).contains("UP");
     }
